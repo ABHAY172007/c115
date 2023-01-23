2e55514f0f5249fbdc72ec5c3be28807d091a648
ABHAY172007 <104831138+ABHAY172007@users.noreply.github.com>
2023-01-23 20:48:21 +0530

    Add files via upload

---
+++ C115-boilerplate-main/C115-boilerplate-main/c115.py
@@ -0,0 +1,4 @@
+import os 
+source='C:/Users/abhay/Downloads/C115-boilerplate-main/C115-boilerplate-main/abc.txt'
+dest='C:/Users/abhay/Downloads/C115-boilerplate-main/C115-boilerplate-main/abh.txt'
+os.rename(source,dest)
